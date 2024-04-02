6c4d3709c664d8545eacbfd688895c9c4a409abb
qoqomi <energyy044@gmail.com>
2024-04-02 22:00:59 +0900

    title 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>seo project</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
