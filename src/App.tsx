647e8882084c5c8bc393328629263dc495077d8d
qoqomi <energyy044@gmail.com>
2024-04-02 21:59:12 +0900

    react-helment-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
